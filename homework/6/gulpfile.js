b3b2e4071d835f03d5c008c1dc5bcbd02cad9f0f
Dmitry <svardmitriy@gmail.com>
2021-12-13 16:46:31 +0600

    saas lessons and homework added

---
+++ homework/6/gulpfile.js
@@ -0,0 +1,33 @@
+﻿//Подключаем локальные пакеты
+var gulp = require("gulp");
+var sass = require("gulp-sass")(require("sass"));
+var print = require("gulp-print");
+
+//Определение задачи по умолчанию в GULP
+// gulp.task("default", function () {
+//   return gulp.src("package.json").pipe(
+//     print(function () {
+//       return "HTTP Server Started";
+//     })
+//   );
+// });
+
+//Определение новой задачи в GULP для SCSS и SASS
+gulp.task("scss", function () {
+  return gulp
+    .src("./samples/styles/main.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest("./samples"));
+});
+
+//Отслеживание изменений
+// gulp.task("scss:watch", function () {
+//   //Находим папку и отмечаем, за каким файлом необходимо наблюдать, если он найден - выполняем задание "scss"
+//   gulp.watch(["./samples/**/*.scss", "./samples/**/*.sass"], ["scss"]);
+// });
+
+gulp.task("watch", function () {
+  gulp.watch("./samples/**/*.scss", gulp.series("scss"));
+});
+
+gulp.task("default", gulp.series(gulp.parallel("watch")));
